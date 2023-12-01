b7f2a2668edd9e7ccae86c5ac3540ddd22a73aeb
korololeh <korol.test90@gmail.com>
2023-12-01 20:06:59 +0200

    add EmailValidator and IncreaseDigitInString solution

--- src/main/java/org/example/EmailValidator.java
+++ src/main/java/org/example/EmailValidator.java
@@ -1,24 +1,28 @@
 package org.example;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class EmailValidator {
     public static void main(String[] args) {
-//        System.out.println(emailValidation("@gmail.com"));
-//        System.out.println(emailValidation("a@gmail.com"));
-//        System.out.println(emailValidation(".@gmail.com"));
-//        System.out.println(emailValidation("agmail.com"));
+        System.out.println(emailValidation("@gmail.com"));
         System.out.println(emailValidation("a@gmail.com"));
-//        String matcher = "[A-Za-z0-9](.*)";
-//        System.out.println(Pattern.compile(matcher).matcher("@gmail.com").matches());
+        System.out.println(emailValidation(".@gmail.com"));
+        System.out.println(emailValidation("agmail.com"));
+        System.out.println(emailValidation("97s@gmail.com"));
+        System.out.println(emailValidation("a@gm@ail.com"));
+        System.out.println(emailValidation("aqwer@gm.qwerail.com"));
+        System.out.println(emailValidation("@gmail.com"));
     }
 
     public static boolean emailValidation(String email) {
         int atIndex = email.indexOf("@");
         int dotLastIndex = email.lastIndexOf(".");
 
-        if (email.chars().filter(x -> x == ' ').count() > 1) {
+        if (email.isEmpty()) {
+            return false;
+        } else if (email.contains(" ")) {
             return false;
         } else if (email.chars().filter(x -> x == '@').count() > 1) {
             return false;
@@ -28,9 +32,9 @@ public class EmailValidator {
             return false;
         } else if (email.indexOf(".") < 1 || atIndex < 1) {
             return false;
-        } else if (email.chars().skip(atIndex).limit(dotLastIndex).distinct().filter(x->!Character.isAlphabetic(x)).count() > 0) {
+        } else if (email.substring(atIndex+1, dotLastIndex-1).chars().distinct().filter(x->!Character.isAlphabetic(x)).count() > 0) {
             return false;
-        } else if (email.chars().skip(dotLastIndex).distinct().filter(x->!Character.isAlphabetic(x)).count() > 0) {
+        } else if (email.substring(dotLastIndex+1).chars().distinct().filter(x->!Character.isAlphabetic(x)).count() > 0) {
             return false;
         } else {
             return true;
